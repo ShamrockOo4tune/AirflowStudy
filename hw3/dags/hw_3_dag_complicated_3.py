cfdbbdf1dc50872ff681396a1462f00488a752ff
Shamil Gumerov <shamusg12345@gmail.com>
2021-04-09 12:18:57 +1100

    updated functions and dag .py files

--- hw3/dags/hw_3_dag_complicated_3.py
+++ hw3/dags/hw_3_dag_complicated_3.py
@@ -1,7 +1,9 @@
 from airflow.models import DAG
 from airflow.operators.python import PythonOperator
 from airflow.operators.bash import BashOperator
-from util.hw3_functions import download_titanic_dataset_2, pivot_dataset_2, mean_fare_per_class_2
+from util.hw3_functions import download_titanic_dataset_2,\
+                               pivot_dataset_3,\
+                               mean_fare_per_class_3
 
 from util.settings import default_settings
 
@@ -18,12 +20,12 @@ with DAG(**default_settings()) as dag:
     
     pivot_titanic_dataset = PythonOperator(
         task_id='pivot_dataset',
-        python_callable=pivot_dataset_2,
+        python_callable=pivot_dataset_3,
         dag=dag,)
     
     mean_fares_titanic_dataset = PythonOperator(
         task_id='mean_fares_dataset',
-        python_callable=mean_fare_per_class_2,
+        python_callable=mean_fare_per_class_3,
         dag=dag,)
         
     last_task = BashOperator(
--- hw3/dags/util/hw3_functions.py
+++ hw3/dags/util/hw3_functions.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 from airflow.hooks.base import BaseHook
-#from airflow.hooks.base_hook import BaseHook
+from airflow.models import Variable
 
 
 def get_path(file_name):
@@ -28,8 +28,43 @@ def pivot_dataset_2(**kwargs):
                                 columns=['Pclass'],
                                 values=['Name'],
                                 aggfunc='count').reset_index()
+
+
+def pivot_dataset_3(**kwargs):
+
+    # вытягиваем датафрейм сериализованный в json из xcom:
+    titanic_json = kwargs['task_instance'].xcom_pull(task_ids='download_titanic_dataset',
+                                                     key='titanic_df')
+    # преобразуем в pandas dataframe и изменяем агрегацией:
+    titanic_df = pd.read_json(titanic_json)
+    df = titanic_df.pivot_table(index=['Sex'],
+                                columns=['Pclass'],
+                                values='Name',
+                                aggfunc='count').reset_index()
+
+    # создаем кастом хук, коннектшн берем из предварительно созданного в UI:
     pg_hook = BaseHook.get_hook('postgres_default')
-    # pg_hook.insert_rows а вот дальше что не знаю
+
+    # имя таблицы в локальной БД предварительно задано в UI в Variables. Извлекаем:
+    pg_table_name = Variable.get('pivot_table_name')
+
+    # перемалываем датафрейм в список кортежей, приводим типы к стандартным (str и int):
+    pg_rows = list(df.to_records(index=False))
+    pg_rows_conv = [(t[0], int(t[1]), int(t[2]), int(t[3])) for t in pg_rows]
+
+    # извлекаем названия полей(колонок) датафрейма и приводим их типы к строковому:
+    pg_columns = list(df.columns)
+    pg_columns_conv = [pg_columns[0],
+                       '"' + str(pg_columns[1]) + '"',
+                       '"' + str(pg_columns[2]) + '"',
+                       '"' + str(pg_columns[3]) + '"']
+
+    # отправляем данные в локальную БД:
+    pg_hook.insert_rows(table=pg_table_name,
+                        rows=pg_rows_conv,
+                        target_fields=pg_columns_conv,
+                        commit_every=0,
+                        replace=False)
 
 
 def mean_fare_per_class_2(**kwargs):
@@ -39,8 +74,39 @@ def mean_fare_per_class_2(**kwargs):
     df = titanic_df.pivot_table(columns=['Pclass'],
                                 values='Fare',
                                 aggfunc='mean').reset_index()
+
+
+def mean_fare_per_class_3(**kwargs):
+
+    # вытягиваем датафрейм сериализованный в json из xcom:
+    titanic_json = kwargs['task_instance'].xcom_pull(task_ids='download_titanic_dataset',
+                                                     key='titanic_df')
+    # преобразуем в pandas dataframe и изменяем группировками, агрегациями:
+    titanic_df = pd.read_json(titanic_json)
+    df = titanic_df\
+        .groupby(['Pclass'])\
+        .agg({'Fare': 'mean'})\
+        .reset_index()
+
+    # создаем кастом хук, коннектшн берем из предварительно созданного в UI:
     pg_hook = BaseHook.get_hook('postgres_default')
-    # pg_hook.insert_rows а вот дальше что не знаю
+
+    # имя тааблицы в локальной БД предварительно задано в UI в Variables. Извлекаем:
+    pg_table_name = Variable.get('mean_fares_table_name')
+
+    # перемалываем датафрейм в список кортежей, приводим типы к стандартным (int и float):
+    pg_rows = list(df.to_records(index=False))
+    pg_rows_conv = [(int(t[0]), float(t[1])) for t in pg_rows]
+
+    # извлекаем названия полей(колонок) датафрейма:
+    pg_columns = list(df.columns)
+
+    # отправляем данные в локальную БД:
+    pg_hook.insert_rows(table=pg_table_name,
+                        rows=pg_rows_conv,
+                        target_fields=pg_columns,
+                        commit_every=0,
+                        replace=False)
 
 
 def pivot_dataset():
